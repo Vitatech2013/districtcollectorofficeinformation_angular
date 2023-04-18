7a82addf984b49894afe4e5f78ab48b5f9d9bb22
NaveenRK45 <naveen45.vita@email.com>
2023-04-18 18:26:51 +0530

    user-addcode 18-04-2023

--- src/app/User-Citizen/citizen-applydeathcertificate/citizen-applydeathcertificate.component.ts
+++ src/app/User-Citizen/citizen-applydeathcertificate/citizen-applydeathcertificate.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./citizen-applydeathcertificate.component.scss']
 })
 export class CitizenApplydeathcertificateComponent {
+  url:any;
+
+  onSelectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.url = event.target?.result;
+      }
+    }
+  }
 
 }
